1891e074e2bd6d131c39874a786227fe55f1cb2b
Vishnu Reddy <74573304+Vishbobjoebob@users.noreply.github.com>
2022-02-03 20:39:15 -0500

    Add files via upload

--- include/71811/opControl.hpp
+++ include/71811/opControl.hpp
@@ -6,4 +6,5 @@ int tilterControl();
 int clampControl();
 int manualControl();
 int boingControl();
+int coastControl();
 void resetEncoders();
---
+++ include/71811/robotConfig.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "api.h"
+
+extern pros::Motor l_motor;
+extern pros::Motor r_motor;
+
+extern pros::Motor arm;
+extern pros::Motor conveyor;
+
+extern pros::ADIDigitalOut clamp1;
+extern pros::ADIDigitalOut leftTilter;
+extern pros::ADIDigitalOut rightTilter;
+extern pros::ADIDigitalOut boing;
